d899bfe05ea9121dee8c4043f82eb44f3c3b9149
Drawner <gtfperry@gmail.com>
2019-07-20 18:08:58 -0500

    1.1.0 three versions of build

--- lib/src/home/view/stock_home.dart
+++ lib/src/home/view/stock_home.dart
@@ -27,38 +27,128 @@ class StockHomeState extends StateMVC<StockHome> {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        drawerDragStartBehavior: DragStartBehavior.down,
-        key: con.scaffoldKey,
-        appBar: con.widget.appBar,
-        floatingActionButton: con.widget.floatingButton.createCompany,
-        drawer: Drawer(
-          child: ListView(
+    return _degree01;
+  }
+
+  Widget get _degree01 => DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          drawerDragStartBehavior: DragStartBehavior.down,
+          key: con.scaffoldKey,
+          appBar: con.widget.appBar,
+          floatingActionButton: con.widget.floatingButton.createCompany,
+          drawer: Drawer(
+            child: ListView(
+              dragStartBehavior: DragStartBehavior.down,
+              children: <Widget>[
+                const DrawerHeader(child: Center(child: Text('Stocks'))),
+                ListTile(
+                  leading: Icon(Icons.assessment),
+                  title: con.title.stockList,
+                  selected: true,
+                ),
+                ListTile(
+                  leading: Icon(Icons.account_balance),
+                  title: con.title.balance,
+                  enabled: false,
+                ),
+                ListTile(
+                  leading: const Icon(Icons.dvr),
+                  title: con.title.dumpConsole,
+                  onTap: con.onTap.dumpConsole,
+                ),
+                const Divider(),
+                ListTile(
+                  leading: const Icon(Icons.thumb_up),
+                  title: con.title.optimistic,
+                  trailing: Radio<StockMode>(
+                    value: StockMode.optimistic,
+                    groupValue: cont.AppStocks.stockMode,
+                    onChanged: con.handleStockModeChange,
+                  ),
+                  onTap: con.onTap.optimistic,
+                ),
+                ListTile(
+                  leading: const Icon(Icons.thumb_down),
+                  title: con.title.pessimistic,
+                  trailing: Radio<StockMode>(
+                    value: StockMode.pessimistic,
+                    groupValue: cont.AppStocks.stockMode,
+                    onChanged: con.handleStockModeChange,
+                  ),
+                  onTap: con.onTap.pessimistic,
+                ),
+                const Divider(),
+                ListTile(
+                  leading: const Icon(Icons.settings),
+                  title: con.title.settings,
+                  onTap: () {
+                    con.onTap.settings(con.context);
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.help),
+                  title: con.title.about,
+                  onTap: () {
+                    con.onTap.about(con.context);
+                  },
+                ),
+              ],
+            ),
+          ),
+          body: TabBarView(
             dragStartBehavior: DragStartBehavior.down,
             children: <Widget>[
-              const DrawerHeader(child: Center(child: Text('Stocks'))),
-              con.widget.stockList,
-              con.widget.accountBalance,
-              con.widget.dumpConsole,
-              const Divider(),
-              con.widget.optimistic,
-              con.widget.pessimistic,
-              const Divider(),
-              con.widget.settings,
-              con.widget.about,
+              con.widget.marketTab,
+              con.widget.portfolioTab,
             ],
           ),
         ),
-        body: TabBarView(
-          dragStartBehavior: DragStartBehavior.down,
-          children: <Widget>[
-            con.widget.marketTab,
-            con.widget.portfolioTab,
-          ],
+      );
+
+  Widget get _degree02 => DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          drawerDragStartBehavior: DragStartBehavior.down,
+          key: con.scaffoldKey,
+          appBar: con.widget.appBar,
+          floatingActionButton: con.widget.floatingButton.createCompany,
+          drawer: Drawer(
+            child: ListView(
+              dragStartBehavior: DragStartBehavior.down,
+              children: <Widget>[
+                const DrawerHeader(child: Center(child: Text('Stocks'))),
+                con.widget.stockList,
+                con.widget.accountBalance,
+                con.widget.dumpConsole,
+                const Divider(),
+                con.widget.optimistic,
+                con.widget.pessimistic,
+                const Divider(),
+                con.widget.settings,
+                con.widget.about,
+              ],
+            ),
+          ),
+          body: TabBarView(
+            dragStartBehavior: DragStartBehavior.down,
+            children: <Widget>[
+              con.widget.marketTab,
+              con.widget.portfolioTab,
+            ],
+          ),
         ),
-      ),
-    );
-  }
+      );
+
+  Widget get _degree03 => DefaultTabController(
+    length: 2,
+    child: Scaffold(
+      drawerDragStartBehavior: DragStartBehavior.down,
+      key: con.scaffoldKey,
+      appBar: con.widget.appBar,
+      floatingActionButton: con.widget.floatingButton.createCompany,
+      drawer: con.widget.drawer,
+      body: con.widget.body,
+    ),
+  );
 }
